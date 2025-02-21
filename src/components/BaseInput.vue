0976bd98b7088de5b29e2291094e0913e8f42a1d
Pavel Kruglyakovskiy <kdpavel8@gmail.com>
2025-02-21 22:33:57 +0500

    feat(BaseInput.vue): create ui component BaseInput

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="base-input">
+    <div class="base-input__wrapper">
+      <input
+        v-model="localValue"
+        v-bind="$attrs"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BaseInput',
+  props: {
+    value: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    localValue: {
+      get () {
+        return this.value
+      },
+      set (value) {
+        this.$emit('input', value)
+      }
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.base-input {
+  input {
+    padding: 0;
+    border: none;
+    background: transparent;
+    &::placeholder {
+      color: #86949E;
+
+      font-weight: 400;
+      font-size: 14px;
+      line-height: 18px;
+      letter-spacing: 0;
+    }
+    &:focus-visible {
+      box-shadow: none;
+      outline: none;
+    }
+  }
+}
+</style>
