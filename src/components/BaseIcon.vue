69c76d2e7ce9108967d2a746fa69d020c9ee7abc
Pavel Kruglyakovskiy <kdpavel8@gmail.com>
2025-02-22 11:44:07 +0500

    feat(BaseIcon.vue): create baseIcon component
    install packages vue-svg-loader and vue-template-compiler

---
+++ src/components/BaseIcon.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="base-icon">
+    <div class="base-icon__wrapper">
+      <component :is="iconComponent" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BaseIcon',
+  props: {
+    icon: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    iconComponent () {
+      try {
+        return require(`@/assets/icons/${this.icon}.svg`)
+      } catch (error) {
+        console.error(`Icon not found: ${this.icon}`)
+        return null
+      }
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss"></style>
