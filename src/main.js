015972d55d4868ff7e1f7fc98214bb98d1ac3ff8
Pavel Kruglyakovskiy <kdpavel8@gmail.com>
2025-02-23 19:06:43 +0500

    dd997eda8e51ddeae267b00e803c88196c0bb657 add main.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import AlertService from '@/plugins/alertService'
+
 import '@/assets/styles/fonts.css'
 import '@/assets/styles/index.scss'
 
 Vue.config.productionTip = false
+
+Vue.use(AlertService)
 
 new Vue({
   render: h => h(App)
