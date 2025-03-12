1afd69083dbbac3e10788fd61d8d0d7b215f1dc7
Pavel Kruglyakovskiy <kdpavel8@gmail.com>
2025-03-12 14:26:51 +0500

    fix(.gitlab-ci.yml, vue.config.js): edit root path from /taskboard to /, change build CI\CD scripts

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
 
-  publicPath: '/taskboard/',
+  publicPath: '/',
 
   chainWebpack: (config) => {
     config.module.rules.delete('svg')
