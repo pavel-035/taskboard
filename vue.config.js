b0d4b378a28dd34708d56ee31a02c31aa3b6a2c0
Pavel Kruglyakovskiy <kdpavel8@gmail.com>
2025-03-12 14:35:59 +0500

    cancel commit 1afd69083dbbac3e10788fd61d8d0d7b215f1dc7

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
 
-  publicPath: '/',
+  publicPath: '/taskboard/',
 
   chainWebpack: (config) => {
     config.module.rules.delete('svg')
